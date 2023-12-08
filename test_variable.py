fd85b5c3a85b273b13859500ab2136349b566599
Midhun <99331155+mpmathew@users.noreply.github.com>
2023-12-08 19:50:54 +0530

    Update test_variable.py

--- test_variable.py
+++ test_variable.py
@@ -1,5 +1,6 @@
 import subprocess
 from airflow import DAG
+from airflow.utils.db import create_session
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
@@ -24,17 +25,21 @@ dag = DAG(
 )
 
 def convert_all_variables_to_environment_variables(**kwargs):
-    # Retrieve all Airflow variables
-    #airflow_variables = Variable.get_variable_names()
-    output = (subprocess.check_output("airflow variables", shell=True)).decode('utf-8').split('pid=')[1].split()[1:-1]
-    print(output)
+    # a db.Session object is used to run queries against
+    # the create_session() method will create (yield) a session
+    with create_session() as session:
+        # By calling .query() with Variable, we are asking the airflow db 
+        # session to return all variables (select * from variables).
+        # The result of this is an iterable item similar to a dict but with a 
+        # slightly different signature (object.key, object.val).
+        airflow_vars = {var.key: var.val for var in session.query(Variable)}
     
-    # Convert each Airflow variable to an OS environment variable
-    # for var_name in airflow_variables:
-    #     var_value = Variable.get(var_name)
-    #     os.environ[var_name] = var_value
-
-    # print("All Airflow variables converted to OS environment variables.")
+        Convert each Airflow variable to an OS environment variable
+        for var_name in airflow_variables:
+            var_value = Variable.get(var_name)
+            os.environ[var_name] = var_value
+    
+        print("All Airflow variables converted to OS environment variables.")
 
 convert_task = PythonOperator(
     task_id='convert_all_variables_task',
