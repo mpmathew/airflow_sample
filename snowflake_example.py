8df8f1c455018c43b989caef9c79a23a99897887
Midhun <99331155+mpmathew@users.noreply.github.com>
2023-12-22 15:34:25 +0530

    Update snowflake_example.py

--- snowflake_example.py
+++ snowflake_example.py
@@ -8,7 +8,7 @@ from airflow.providers.common.sql.operators.sql import (
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
 from pendulum import datetime
 from airflow.utils.task_group import TaskGroup
-from . import test_sql as sql_stmts
+import test_sql as sql_stmts
 
 SNOWFLAKE_FORESTFIRE_TABLE = "forestfires"
 SNOWFLAKE_COST_TABLE = "costs"
