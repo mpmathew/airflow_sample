26f088e54f3156c0734dd73a6585684698578312
Midhun <99331155+mpmathew@users.noreply.github.com>
2023-12-22 16:31:23 +0530

    Update test_sql.py

--- test_sql.py
+++ test_sql.py
@@ -1,4 +1,4 @@
-create_forestfire_table = ["""USE PUBLIC;""",
+create_forestfire_table = ["""USE TEST_DEV_DB.PUBLIC;""",
                            """
     CREATE OR REPLACE TRANSIENT TABLE {{ params.table_name }}
         (
