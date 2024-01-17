bd3dc39429235b70795a10f04f9b7b468bc8fcf2
Midhun <99331155+mpmathew@users.noreply.github.com>
2024-01-17 15:47:24 +0530

    Create jaffle_shop_demo.py

---
+++ jaffle_shop_demo.py
@@ -0,0 +1,78 @@
+from pendulum import datetime
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from cosmos import DbtTaskGroup, RenderConfig
+from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig
+from pathlib import Path
+
+
+profile_config = ProfileConfig(
+    profile_name="jaffle_shop",
+    target_name="dev",
+    profiles_yml_filepath = "/appz/home/airflow/dags/dbt/jaffle_shop/profiles.yml",
+)
+
+
+with DAG(
+    dag_id="jaffle_shop_new",
+    start_date=datetime(2023, 11, 10),
+    schedule_interval="0 0 * 1 *",
+):
+    e1 = EmptyOperator(task_id="pre_dbt")
+
+    seeds_tg = DbtTaskGroup(
+        project_config=ProjectConfig(
+        Path("/appz/home/airflow/dags/dbt/jaffle_shop"),
+    ),
+        operator_args={
+            "append_env": True,
+        },
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(
+        dbt_executable_path="/dbt_venv/bin/dbt",
+    ),
+        render_config=RenderConfig(
+        select=["path:seeds/"],
+    ),
+        default_args={"retries": 2},
+        group_id = "dbt_seeds_group"
+    )
+
+    stg_tg = DbtTaskGroup(
+        project_config=ProjectConfig(
+        Path("/appz/home/airflow/dags/dbt/jaffle_shop"),
+    ),
+        operator_args={
+            "append_env": True,
+        },
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(
+        dbt_executable_path="/dbt_venv/bin/dbt",
+    ),
+        render_config=RenderConfig(
+        select=["path:models/staging/"],
+    ),
+        default_args={"retries": 2},
+        group_id = "dbt_stg_group"
+    )
+
+    dbt_tg = DbtTaskGroup(
+        project_config=ProjectConfig(
+        Path("/appz/home/airflow/dags/dbt/jaffle_shop"),
+    ),
+        operator_args={
+            "append_env": True,
+        },
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(
+        dbt_executable_path="/dbt_venv/bin/dbt",
+    ),
+        render_config=RenderConfig(
+        exclude=["path:models/staging","path:seeds/"],
+    ),
+        default_args={"retries": 2},
+    )
+    
+    e2 = EmptyOperator(task_id="post_dbt")
+
+    e1 >> seeds_tg >> stg_tg >> dbt_tg >> e2
