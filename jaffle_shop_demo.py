2595aec0965aa87d0f39a379a1ba978bf8e27741
Midhun <99331155+mpmathew@users.noreply.github.com>
2024-01-17 15:47:56 +0530

    Update jaffle_shop_demo.py

--- jaffle_shop_demo.py
+++ jaffle_shop_demo.py
@@ -14,7 +14,7 @@ profile_config = ProfileConfig(
 
 
 with DAG(
-    dag_id="jaffle_shop_new",
+    dag_id="jaffle_shop_demo",
     start_date=datetime(2023, 11, 10),
     schedule_interval="0 0 * 1 *",
 ):
