49eb05301d74efae1ef85d9ea88e58e150235621
Midhun <99331155+mpmathew@users.noreply.github.com>
2024-01-17 17:11:59 +0530

    Update jaffle_shop_demo.py

--- jaffle_shop_demo.py
+++ jaffle_shop_demo.py
@@ -16,7 +16,7 @@ profile_config = ProfileConfig(
 with DAG(
     dag_id="jaffle_shop_demo",
     start_date=datetime(2023, 11, 10),
-    schedule_interval="0 0 * 1 *",
+    schedule_interval="0 0 1 * *",
     tags=["example","demo"],
 ):
     e1 = EmptyOperator(task_id="pre_dbt")
