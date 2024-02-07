b018bad8d05b4a27352db669857477f21c4eb30a
Midhun <99331155+mpmathew@users.noreply.github.com>
2024-02-07 17:00:32 +0530

    Update objects_seq.py

--- objects_seq.py
+++ objects_seq.py
@@ -23,34 +23,36 @@ dag = DAG(
 
 # Base directory path
 base_directory_path = "/appz/home/airflow/dags/dbt/jaffle_shop/objects/"
+target_subdirs = ['functions', 'stored_proc', 'streams']
 
-subdirectories = ['functions', 'stored_proc', 'streams']
-
-for subdir_name in subdirectories:
-    subdir_path = os.path.join(base_directory_path, subdir_name)
-    
+for subdir, dirs, files in os.walk(base_directory_path):
+    subdir_name = os.path.basename(subdir)
+    if subdir_name not in target_subdirs:
+        continue
+      
     with TaskGroup(group_id=subdir_name, dag=dag) as tg:
         prev_task = None
         
-        # Ensure the directory exists before attempting to iterate over its contents
-        if os.path.exists(subdir_path) and os.path.isdir(subdir_path):
-            for file in sorted(os.listdir(subdir_path)):
-                if file.endswith('.sql'):
-                    file_path = os.path.join(subdir_path, file)
-                    task_id = f"{subdir_name}_{file.replace('.sql', '')}"
+        for file in sorted(files):
+            if file.endswith('.sql'):
+                file_path = os.path.join(subdir, file)
+                task_id = f"{subdir_name}_{file.replace('.sql', '')}"
+                
+                with open(file_path, 'r') as f:
+                    sql_query = f.read()
+                    
                     task = SnowflakeOperator(
                         task_id=task_id,
-                        sql=file_path,
+                        sql=sql_query,
                         snowflake_conn_id=SNOWFLAKE_CONN_ID,
                         params={"schema_name": SNOWFLAKE_SCHEMA},
                         dag=dag,
-                        )
-                        
+                    )
+                    
                     if prev_task:
-                        prev_task >> task
+                        prev_task >> task  # Set dependency for sequential execution
+                    
                     prev_task = task
-        else:
-            print(f"Directory {subdir_path} does not exist.")
 
 Functions = dag.task_group_dict['functions']
 Procedures = dag.task_group_dict['streams']
