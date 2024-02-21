f4dd2330230826f01c43b94bb93062870d9472ac
Midhun <99331155+mpmathew@users.noreply.github.com>
2024-02-21 10:46:51 +0530

    Update jaffle_shop_demo.py

--- jaffle_shop_demo.py
+++ jaffle_shop_demo.py
@@ -17,7 +17,7 @@ with DAG(
     dag_id="jaffle_shop_demo",
     start_date=datetime(2023, 11, 10),
     schedule_interval="0 0 1 * *",
-    tags=["example","demo"],
+    tags=["mpmathew","demo"],
     default_args = {
     "owner": "mpmathew"
     },
