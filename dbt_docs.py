f30c4fd99f1ab601afa3e7a82209d5f95d315139
Midhun <99331155+mpmathew@users.noreply.github.com>
2024-02-22 12:28:39 +0530

    Create dbt_docs.py

---
+++ dbt_docs.py
@@ -0,0 +1,65 @@
+from pendulum import datetime
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from cosmos import DbtTaskGroup, RenderConfig, LoadMode
+from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig
+from cosmos.constants import TestBehavior
+from airflow.models import Variable
+from pathlib import Path
+from cosmos.operators import DbtDocsOperator
+
+AIRFLOW_USER = "airflow"
+POSTGRES_TEST_PASSWORD = Variable.get("AIRFLOW_POSTGRES_TEST_PASSWORD")
+
+profile_config = ProfileConfig(
+    profile_name="jaffle_shop",
+    target_name="dev",
+    profiles_yml_filepath = "/appz/home/airflow/dags/dbt/jaffle_shop/profiles.yml",
+)
+
+
+with DAG(
+    dag_id="dbt_docs",
+    start_date=datetime(2023, 11, 10),
+    schedule=None,
+    tags=["mpmathew","docs"],
+    default_args = {
+    "owner": "mpmathew"
+    },
+    catchup=False,
+):
+    e1 = EmptyOperator(task_id="pre_dbt")
+
+    seeds_tg = DbtTaskGroup(
+        project_config=ProjectConfig(
+        dbt_project_path=Path("/appz/home/airflow/dags/dbt/jaffle_shop"),
+        env_vars={"AIRFLOW_POSTGRES_TEST_USER": AIRFLOW_USER,"AIRFLOW_POSTGRES_TEST_PASSWORD": POSTGRES_TEST_PASSWORD},
+    ),
+        # operator_args={
+        #     "append_env": True,
+        # },
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(
+        dbt_executable_path="/dbt_venv/bin/dbt",
+    ),
+        render_config=RenderConfig(
+        load_method=LoadMode.DBT_LS,
+        select=["path:seeds/"],
+        test_behavior=TestBehavior.NONE,
+    ),
+        default_args={"retries": 2},
+        group_id = "dbt_seeds_group"
+    )
+  
+    generate_dbt_docs = DbtDocsOperator(
+        task_id="generate_dbt_docs",
+        project_dir="/appz/home/airflow/dags/dbt/jaffle_shop",
+        profile_config=profile_config,
+        # docs-specific arguments
+        # callback=upload_docs,
+    )
+
+    
+    e2 = EmptyOperator(task_id="post_dbt")
+
+    e1 >> generate_dbt_docs >> e2
