ef1c3aad7ca0295d645012007a2b3aa2d364194b
Midhun <99331155+mpmathew@users.noreply.github.com>
2024-04-04 11:47:57 +0530

    Update jaffle_shop_demo.py

--- jaffle_shop_demo.py
+++ jaffle_shop_demo.py
@@ -26,7 +26,7 @@ with DAG(
     tags=["mpmathew","demo"],
     start_date=pendulum.datetime(2024, 1, 1, tz="America/Los_Angeles"),
     default_args=default_args,
-    schedule=USWorkdayTimetable(Time(4,30)),
+    schedule=USWorkdayTimetable(Time(2,25)),
     catchup=False,
 ):
     e1 = EmptyOperator(task_id="pre_dbt")
